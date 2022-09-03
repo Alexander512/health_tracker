80e56dcc5a6621272ca6ae0f80a45f27a38cd9dc
Alexander Stoisolovich <alexander.stoisolovich@gmail.com>
2022-09-03 18:33:18 -0400

    Added component for listing measures

---
+++ client/store/measure.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+/**
+ * ACTION TYPES
+ */
+const GET_MEASURES = 'GET_MEASURES';
+
+/**
+ * ACTION CREATORS
+ */
+const _getMeasures = (measures) => {
+  return {
+    type: GET_MEASURES,
+    measures
+  };
+};
+
+/**
+ * THUNK CREATORS
+ */
+export const getMeasures = () => {
+  return async (dispatch) => {
+    const response = await axios.get('/api/measures');
+    const measures = response.data;
+    dispatch(_getMeasures(measures));
+  };
+};
+
+/**
+ * REDUCER
+ */
+export default function(state = [], action) {
+  switch (action.type) {
+    case GET_MEASURES:
+      return action.measures
+    default:
+      return state
+  }
+}
